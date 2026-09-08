b05bef84eaaca9aa157ccbb0d1694d5c3e174970
Floffy <vraoofabciy@gmail.com>
2026-09-08 23:52:49 +0300

    Make first task "Larp year calculator"

---
+++ OOP_Lab_2/Leap_year.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace OOP_Lab_2
+{
+    internal class Leap_year
+    {
+        //Розробити програму, яка буде питати у користувача рік, після чого буде відповідати чи високосний це рік
+        //Для того щоб рік був високосний, він повинен підтримувати наступні вимоги:
+
+        //кожен 4ий рік - високосний
+        //кожен 100ий рік - НЕ високосний
+        //кожен 400ий рік - високосний
+
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.InputEncoding = Encoding.UTF8;
+
+                Console.Write("Введіть рік: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine($"Рік {year} є високосним.");
+                }
+                else if (year % 100 == 0)
+                {
+                    Console.WriteLine($"Рік {year} НЕ є високосним.");
+                }
+                else if (year % 400 == 0)
+                {
+                    Console.WriteLine($"Рік {year} є високосним.");
+                }
+                else
+                {
+                    Console.WriteLine($"Рік {year} НЕ є високосним.");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
